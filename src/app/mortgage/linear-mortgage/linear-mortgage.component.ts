6be843caa9653dc9ae895b5b57c55f4f54c0ed40
Jonathan <jonathanoudshoorn@gmail.com>
2019-06-09 16:38:30 +0200

    added chart.js example

--- src/app/mortgage/linear-mortgage/linear-mortgage.component.ts
+++ src/app/mortgage/linear-mortgage/linear-mortgage.component.ts
@@ -1,17 +1,54 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Observable, } from 'rxjs';
 import { MonthlyPayment } from '../../shared/models/mortgage-payment-month.model';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'cash-linear-mortgage',
   templateUrl: './linear-mortgage.component.html',
   styleUrls: ['./linear-mortgage.component.scss']
 })
-export class LinearMortgageComponent implements OnInit {
+export class LinearMortgageComponent implements OnInit, AfterViewInit {
 
   @Input() public mortgageMonthlyPredictions$: Observable<MonthlyPayment[]>;
 
+  @ViewChild('linearMortgageChart', {static: false}) graphCanvas: ElementRef;
+  public context: CanvasRenderingContext2D;
+
+  public mortgageChart: Chart;
+
   constructor() { }
+
+  public ngAfterViewInit(): void {
+    this.context = (this.graphCanvas.nativeElement).getContext('2d');
+
+    this.mortgageChart = new Chart(this.context, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'], // your labels array
+        datasets: [
+          {
+            data: [12, 19, 3, 5, 2, 3], // your data array
+            borderColor: '#00AEFF',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+  }
 
   public ngOnInit() {
 
