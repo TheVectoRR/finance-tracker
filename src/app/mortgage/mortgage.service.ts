d1775ff1759a176d40122982cae7b72cf50e4afb
Jonathan <jonathanoudshoorn@gmail.com>
2019-06-14 10:23:00 +0200

    get extra redemption payment per month (still to be calculated in graph)

--- src/app/mortgage/mortgage.service.ts
+++ src/app/mortgage/mortgage.service.ts
@@ -24,7 +24,11 @@ export class MortgageService {
             totalAmount: data.totalAmount,
             lengthInYears: data.lengthInYears,
             startingDate: moment(data.startingDate, 'DD-MM-YYYY'),
-            fixedInterests: data.fixedInterests
+            fixedInterests: data.fixedInterests,
+            extraRedemptions: data.extraRedemptions.map((redemption) => ({
+              dateOfRedemption: moment(redemption.dateOfRedemption, 'DD-MM-YYYY'),
+              redemptionAmount: redemption.redemptionAmount
+            }))
           }))
         );
       }
@@ -40,6 +44,7 @@ export class MortgageService {
           startingDate: mortgageData.startingDate,
           fixedInterestRate: mortgageData.fixedInterests,
           redemptionAmountPerMonth: mortgageData.totalAmount / (mortgageData.lengthInYears * MONTHS_IN_YEAR),
+          extraRedemption: mortgageData.extraRedemptions
         }))
       );
 
@@ -48,17 +53,24 @@ export class MortgageService {
           const monthArray: MonthlyPayment[] = [];
 
           for (let monthCtr = 0; monthCtr < mortgageData.lengthInMonths; monthCtr++) {
+            const currentMonth = mortgageData.startingDate.add(1, 'month').format('MM-YYYY');
+
             const interestRateForPeriod = this.getInterestRateForMonthNumber(monthCtr, mortgageData.fixedInterestRate);
             const rentPayment = (interestRateForPeriod / 100 *
               (mortgageData.totalCapitalToPay - mortgageData.redemptionAmountPerMonth * monthCtr)) / MONTHS_IN_YEAR;
 
+            const extraRedemptionMonth = mortgageData.extraRedemption
+              .find((extraRed) => (extraRed.dateOfRedemption.format('MM-YYYY') === currentMonth));
+
+            const extraRedemption = extraRedemptionMonth ? extraRedemptionMonth.redemptionAmount : 0;
+
             monthArray.push({
-              monthTitle: mortgageData.startingDate.add(1, 'month').format('MM-YYYY'),
+              monthTitle: currentMonth,
               monthlyCapitalPayment: mortgageData.redemptionAmountPerMonth,
               monthlyRentPayment: rentPayment,
               totalToPayThisMonth: mortgageData.redemptionAmountPerMonth + rentPayment,
               totalCapitalStillToPay: mortgageData.totalCapitalToPay - mortgageData.redemptionAmountPerMonth * (monthCtr + 1),
-              extraRedemptionPaymentThisMonth: 0
+              extraRedemptionPaymentThisMonth: extraRedemption
             });
           }
 
@@ -69,19 +81,27 @@ export class MortgageService {
 
     private getInterestRateForMonthNumber(monthNumber: number, interestRateArray: FixedInterest[]): number {
       return interestRateArray
-        .reduce((period1, period2) => monthNumber <= period1.lengthInYears * MONTHS_IN_YEAR ? period1 : period2).interestRate;
+        .reduce((period1, period2) => (monthNumber <= period1.lengthInYears * MONTHS_IN_YEAR) ? period1 : period2)
+        .interestRate;
     }
 }
 
-export interface MortgageResponse {
+interface MortgageResponse {
   totalAmount: number;
   lengthInYears: number;
   startingDate: moment.Moment;
   fixedInterests: FixedInterest[];
+  extraRedemptions: ExtraRedemption[];
 }
 
-export interface FixedInterest {
+interface FixedInterest {
   interestRate: number;
   lengthInYears: number;
 }
+
+interface ExtraRedemption {
+  dateOfRedemption: moment.Moment;
+  redemptionAmount: number;
+}
+
 
